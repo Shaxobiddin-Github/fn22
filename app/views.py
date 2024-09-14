874245a97aa9260f8f4a95efa35b56ec3e8c3a9f
shaxobiddin <shaxobiddinnormatov2@gmail.com>
2024-09-14 16:43:09 +0500

    uzgarishlar

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Category, News
 
 
 def index(request):
-    return render(request, "app/index.html")
+    newsers = News.objects.filter(is_active = True)
+    news_banner = newsers.filter(is_banner=True).last()
+    news_top_story = newsers.order_by("-views").first()
+    latest_news = newsers.order_by("-created")[:8]
+    context = {
+        "news_banner":news_banner,
+        "news_top_story":news_top_story,
+        "latest_news":latest_news,
+    }
+    return render(request, "app/index.html",context) 
