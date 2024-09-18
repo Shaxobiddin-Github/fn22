2ef065c1666ebab4b9b7025c96390481b4f14aa4
shaxobiddin <shaxobiddinnormatov2@gmail.com>
2024-09-18 22:39:21 +0500

    5-dars uzgarishlari

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import index,detail
+from .views import index,detail,register,user_login,user_logout
 
 urlpatterns = [
     path('', index, name='home'),
     path('detail/<int:pk>/', detail, name='detail'),
+    path('register/',register, name='register'),
+    path('login/',user_login, name='login'),
+    path('logout/',user_logout, name='logout'),
+    # path('search/', search, name='search'),
+
     
 ]
